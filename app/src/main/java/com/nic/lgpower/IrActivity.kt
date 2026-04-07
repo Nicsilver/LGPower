54fd47ec962489cd8318421c2354e6d7b820a795
Nicolaj Jensen <nic.silver@hotmail.dk>
2026-04-08 00:56:12 +0200

    Add haptic feedback to widgets via Vibrator API performHapticFeedback() doesn't work in windowless activities - use Vibrator.vibrate() directly instead. Added VIBRATE permission to manifest.

--- app/src/main/java/com/nic/lgpower/IrActivity.kt
+++ app/src/main/java/com/nic/lgpower/IrActivity.kt
@@ -3,11 +3,15 @@ package com.nic.lgpower
 import android.app.Activity
 import android.hardware.ConsumerIrManager
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
 
 class IrActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        getSystemService(Vibrator::class.java)
+            ?.vibrate(VibrationEffect.createOneShot(50, VibrationEffect.DEFAULT_AMPLITUDE))
         val irManager = getSystemService(CONSUMER_IR_SERVICE) as? ConsumerIrManager
         if (irManager?.hasIrEmitter() == true) {
             val pattern = intent.getIntArrayExtra(EXTRA_PATTERN) ?: LGPowerWidget.LG_POWER_PATTERN
